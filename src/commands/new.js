de4d9994fed8807146ee7ba3897ad9105ce52806
Matt Ma <qma@walmartlabs.com>
2014-07-03 21:15:01 -0700

    working on the Command - new, create a new directory

--- src/commands/new.js
+++ src/commands/new.js
@@ -1,7 +1,37 @@
+var fs = require('../utils/fs');
+var msg = require('../utils/message');
+//var appDirs = require('../utils/appDirs');
+
 var create = function(dirName, options) {
-  console.log('should create a new app folder');
-  console.log('dirName: ', dirName);
-  console.log('options: ', options);
+  var paths = create.getPaths.apply(null, arguments);
+  if (!paths) msg.error("See 'em new --help'.");
+
+  // Create a new directory name what user passed in
+  fs.mkdirpSync(paths.app);
+
+  msg.notify("\n-> All done! Your Ember App ( " + paths.app + " ) has been successfully created!");
 };
 
 module.exports = create;
+
+create.getPaths = function(appPath, env) {
+  if (arguments.length > 2) return false;
+  if (arguments.length == 1) {
+    env = appPath;
+    appPath = '.';
+  }
+  return {
+    app: appPath
+  };
+};
+
+function copyVendor(paths) {
+  var src = __dirname+'/../../packages';
+  var dest = paths.js+'/vendor';
+  fs.readdirSync(src).forEach(function(filePath) {
+    var file = fs.readFileSync(src+'/'+filePath).toString();
+    fs.writeFileSync(dest+'/'+filePath, file);
+  });
+}
+
+
---
+++ src/utils/appDirs.js
@@ -0,0 +1,12 @@
+module.exports = [
+  'components',
+  'config',
+  'controllers',
+  'helpers',
+  'models',
+  'routes',
+  'templates',
+  'views',
+  'mixins'
+];
+
