79649cbd38f069847e7c4c0ea6969221e036ab2d
Matt Ma <matt@mattmadesign.com>
2014-07-27 20:17:13 -0700

    update the model generator info

--- src/skeletons/generators/model.js
+++ src/skeletons/generators/model.js
@@ -1,10 +1,15 @@
 import DS from 'ember-data';
 
-// var attr = DS.attr;
+// http://emberjs.com/guides/models/defining-models/
+// var attr = DS.attr,
+//   hasMany = DS.hasMany,
+//   belongsTo = DS.belongsTo;
 
 var __NAMESPACE__ = DS.Model.extend({
+  // number: attr(),
   // title: attr('string'),
-  // isCompleted: attr('boolean')
+  // isCompleted: attr('boolean'),
+  // others: hasMany('other')
 });
 
 export default __NAMESPACE__;
