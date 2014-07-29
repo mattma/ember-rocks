91b3a7644b1ebc6e44fb20e1b58842e9a938db4f
Matt Ma <matt@mattmadesign.com>
2014-07-29 13:28:18 -0700

    clean up the server side code, handle the html5 push state if possible

--- src/skeletons/core/server/app.js
+++ src/skeletons/core/server/app.js
@@ -1,88 +1,88 @@
 module.exports = function(options) {
-	var PUBLIC_PATH, VIEWS_PATH, app, express, fs, emberApp, sysPath, env, logger, favicon, cookieParser, bodyParser;
+  var PUBLIC_PATH, VIEWS_PATH, app, express, fs, emberApp, sysPath, env, logger, favicon, cookieParser, bodyParser;
 
-	express = require('express');
-	sysPath = require('path');
-	fs = require('fs');
-	logger = require('morgan');
-	favicon = require('static-favicon');
-	cookieParser = require('cookie-parser');
-	bodyParser = require('body-parser');
+  express = require('express');
+  sysPath = require('path');
+  fs = require('fs');
+  logger = require('morgan');
+  favicon = require('static-favicon');
+  cookieParser = require('cookie-parser');
+  bodyParser = require('body-parser');
 
-	app = module.exports = express();
-	emberApp = 'client';
-	PUBLIC_PATH = sysPath.join(__dirname, '..', emberApp);
-	VIEWS_PATH = sysPath.join(__dirname, '..', 'server/views');
+  app = module.exports = express();
+  emberApp = 'client';
+  PUBLIC_PATH = sysPath.join(__dirname, '..', emberApp);
+  VIEWS_PATH = sysPath.join(__dirname, '..', 'server/views');
 
-	env = process.env.NODE_ENV || 'development';
+  env = process.env.NODE_ENV || 'development';
 
-	var homeRoute = require('./routes/index');
+  var rootRoute = require('./routes/index');
 
-	app.set('view engine', 'jade');
-	app.set('views', VIEWS_PATH);
-	app.use(favicon());
-	app.use(logger('dev'));
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded());
-	app.use(cookieParser());
-	//app.use(express.methodOverride());
-	app.use(express.static(PUBLIC_PATH));
+  app.set('view engine', 'jade');
+  app.set('views', VIEWS_PATH);
+  app.use(favicon());
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded());
+  app.use(cookieParser());
+  //app.use(express.methodOverride());
+  app.use(express.static(PUBLIC_PATH));
 
-      // Route General
-	app.use('/', homeRoute);
+  // Need to be deleted in development
+  // GET mocks data from /server/mocks/people.json
+  var fakeUsersRoute = require('./routes/mock_users');
+  app.get('/api/:query', fakeUsersRoute.all);
 
-      // Need to be deleted in development
-      // GET mocks data from /server/mocks/people.json
-      var fakeUsersRoute = require('./routes/mock_users');
-      app.get('/api/:query', fakeUsersRoute.all);
+  // Route General
+  app.use('/', rootRoute);
 
-	/// catch 404 and forward to error handler
-	app.use(function(req, res, next) {
-	    var err = new Error('Not Found');
-	    err.status = 404;
-	    next(err);
-	});
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+  });
 
-	/// error handlers
+  // error handlers
 
-	// development error handler
-	// will print stacktrace
-	if (app.get('env') === 'development') {
-		if (process.argv[3]) {
-			options = {
-				host: process.argv[2],
-				port: process.argv[3]
-			};
-		} else {
-			options = {
-				host: 'localhost',
-				port: 3001
-			};
-		}
-	    app.use(function(err, req, res, next) {
-	        res.status(err.status || 500);
-	        res.render('error', {
-	            message: err.message,
-	            error: err
-	        });
-	    });
-	}
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    if (process.argv[3]) {
+      options = {
+        host: process.argv[2],
+        port: process.argv[3]
+      };
+    } else {
+      options = {
+        host: 'localhost',
+        port: 3001
+      };
+    }
+      app.use(function(err, req, res, next) {
+          res.status(err.status || 500);
+          res.render('error', {
+              message: err.message,
+              error: err
+          });
+      });
+  }
 
-	// production error handler
-	// no stacktraces leaked to user
-	app.use(function(err, req, res, next) {
-	    res.status(err.status || 500);
-	    res.render('error', {
-	        message: err.message,
-	        error: {}
-	    });
-	});
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+          message: err.message,
+          error: {}
+      });
+  });
 
-	return app.listen(options.port, function() {
-		return console.log("Starting web server on port " + options.port + " in " + app.locals.settings.env + " mode");
-	}).on("error", function(err) {
-		if (err.code === 'EADDRINUSE') {
-			return console.log("Port " + options.port + "  is already in use by another process.");
-		}
-	});
+  return app.listen(options.port, function() {
+    return console.log("Starting web server on port " + options.port + " in " + app.locals.settings.env + " mode");
+  }).on("error", function(err) {
+    if (err.code === 'EADDRINUSE') {
+      return console.log("Port " + options.port + "  is already in use by another process.");
+    }
+  });
 };
--- src/skeletons/core/server/routes/index.js
+++ src/skeletons/core/server/routes/index.js
@@ -1,8 +1,18 @@
 var express = require('express'),
 	router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
+// http://expressjs.com/4x/api.html#router
+
+// this route uses the ":user" named parameter
+// which will cause the 'user' param callback to be triggered
+// GET /users/assets/styles/reset.css 304 12ms
+router.get('/users/:user', function(req, res, next) {
+  console.log('@TODO: need to handle the params here');
+  next();
+});
+
+// always invoked
+router.get('*', function(req, res) {
 	return res.render('base');
 });
 
