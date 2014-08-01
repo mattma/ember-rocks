790c8788c0a93e5c486f8d83647b7ceed5048f11
Matt Ma <matt@mattmadesign.com>
2014-08-01 13:48:10 -0700

    temp fix on navigation position and remote the additional query params route in server side

--- src/skeletons/core/server/routes/index.js
+++ src/skeletons/core/server/routes/index.js
@@ -1,19 +1,11 @@
 var express = require('express'),
-	router = express.Router();
+  router = express.Router();
 
 // http://expressjs.com/4x/api.html#router
 
-// this route uses the ":user" named parameter
-// which will cause the 'user' param callback to be triggered
-// GET /users/assets/styles/reset.css 304 12ms
-router.get('/users/:user', function(req, res, next) {
-  console.log('@TODO: need to handle the params here');
-  next();
-});
-
 // always invoked
 router.get('*', function(req, res) {
-	return res.render('base');
+  return res.render('base');
 });
 
 module.exports = router;
