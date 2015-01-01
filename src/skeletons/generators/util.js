2b0c9be6b7a60b4286bf1840d14bcb24d0746b7b
Matt Ma <bigmabig@gmail.com>
2014-12-31 20:06:42 -0800

    update the utils generator with latest improved template

--- src/skeletons/generators/util.js
+++ src/skeletons/generators/util.js
@@ -1,3 +1,15 @@
-export default function __NAMESPACE__() {
-  return true;
-}
+import Ember from 'ember';
+
+var __NAMESPACE__ = Ember.Object.extend({
+
+});
+
+export default __NAMESPACE__;
+
+// function __NAMESPACE__ () {
+//
+// }
+
+// export {
+//  __NAMESPACE__
+// }
