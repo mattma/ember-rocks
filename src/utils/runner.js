768425ca85b152546e81ce071685d7719fed65a0
Matt Ma <bigmabig@gmail.com>
2015-01-21 15:39:02 -0800

    update the runner with new v8flag from liftOff

--- src/utils/runner.js
+++ src/utils/runner.js
@@ -9,18 +9,18 @@ var argv = require('minimist')(process.argv.slice(2)),
 
 var runner = function(cb, command) {
   var G = new Liftoff({
-    name: 'gulp'
-  });
-
-  G.on('require', function (name) {
-    gutil.log(gutil.colors.cyan('[-Loading:] ' + name));
-  }).on('requireFail', function (name, err) {
-    gutil.log(
-      gutil.colors.red('[-Error:] Unable to load:'),
-      gutil.colors.red(name),
-      gutil.colors.red(err)
-    );
-  });
+    name: 'em',
+    v8flags: ['--harmony'] // to support all flags: require('v8flags');
+  })
+    .on('require', function (name) {
+      gutil.log(gutil.colors.cyan('[-Loading:] ' + name));
+    }).on('requireFail', function (name, err) {
+      gutil.log(
+        gutil.colors.red('[-Error:] Unable to load:'),
+        gutil.colors.red(name),
+        gutil.colors.red(err)
+      );
+    });
 
   // attach the gulp task name into the cb namespace
   // it will be resolved during the run time
@@ -30,7 +30,8 @@ var runner = function(cb, command) {
     cwd: argv.cwd,
     configPath: argv.gulpfile,
     require: argv.require,
-    completion: argv.completion
+    completion: argv.completion,
+    verbose: argv.verbose
   }, cb );
 };
 
