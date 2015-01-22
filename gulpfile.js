a8ed688c42de21df5f3d8ab94f77718314b9d182
Matt Ma <bigmabig@gmail.com>
2015-01-21 16:00:41 -0800

    add a new rule of jscs to handle code quality

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var gulp = require('gulp'),
-  log = require('gulp-util').log,
-  jshint = require('gulp-jshint');
+var gulp = require('gulp');
+var log = require('gulp-util').log;
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 
 var lintFiles = [
   'bin/em',
@@ -19,6 +20,21 @@ gulp.task('lint', function() {
   return gulp.src(lintFiles)
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'));
+});
+
+// task: jscs
+// @describe need to be passed to be able to build the project
+gulp.task('jscs', function () {
+  var src = [
+    'gulpfile.js',
+    'test/**/*.js',
+    'src/**/*.js'
+  ];
+
+  return gulp.src(src)
+    .pipe(jscs({
+      configPath: './.jscsrc'
+    }));
 });
 
 gulp.task('watch', function() {
