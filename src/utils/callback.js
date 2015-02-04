23379dabae0da817c09ac0e6935774e538b6adae
Matt Ma <bigmabig@gmail.com>
2015-02-03 17:05:15 -0800

    clean up the callback.js

--- src/utils/callback.js
+++ src/utils/callback.js
@@ -7,9 +7,7 @@ var semver = require('semver');
 var path = require('path');
 var logEvents = require('./logEvents');
 
-// callback is loaded `env` which passed from `LiftOff` instance
-// you could tweak the `env` properties in `runner.js` `G.launch(opts, cb);`
-function callback (env) {
+function validEmberRockProject (env) {
   if (!env.modulePath && !env.configPath) {
     gutil.log(
       gutil.colors.red('[-Error:] This project may not be created by \'Ember-Rocks\'\n'),
@@ -18,21 +16,25 @@ function callback (env) {
     );
     process.exit(1);
   }
+}
 
+function validGulpVersion (env) {
   var localGulpPackagePath = path.resolve(env.cwd, 'node_modules/gulp/package');
   var localGulpPackage = require(localGulpPackagePath);
 
-  // check for semver difference between global and local installation
   if (semver.gt(localGulpPackage.version, env.modulePackage.version)) {
-    gutil.log(
-      gutil.colors.red('[-Error:] gulp version mismatch!')
-    );
-    gutil.log(
-      gutil.colors.bold(
-        '[-Error:] Submit an issue @ https://github.com/mattma/ember-rocks/issues'
-      )
-    );
+    gutil.log(gutil.colors.red('[-Error:] gulp version mismatch!'));
+    gutil.log(gutil.colors.bold(
+      '[-Error:] Submit an issue @ https://github.com/mattma/ember-rocks/issues'
+    ));
   }
+}
+
+// callback is loaded `env` which passed from `LiftOff` instance
+// you could tweak the `env` properties in `runner.js` `G.launch(opts, cb);`
+function callback (env) {
+  validEmberRockProject(env);
+  validGulpVersion(env);
 
   // this is what actually loads up the gulpfile
   require(env.configPath);
